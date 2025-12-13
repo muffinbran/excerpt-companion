86b97256ac8e23487a2fadec1961c14e8e079d82
muffinbran <raisinbran8241@gmail.com>
2025-12-13 12:02:22 -0800

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-[100dvh] h-[100dvh] w-full text-gray-100 md:p-8 md:min-h-screen md:h-auto">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
